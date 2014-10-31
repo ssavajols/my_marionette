03845476fe77c80302a74cc3f7f962edb5da5249
s.savajols@mazarinedigital.com <s.savajols@gmail.com>
2014-10-31 09:52:41 +0100

    Add twig as template's engine

--- preprocess/app/application/component/sample/view/sample_layout.js
+++ preprocess/app/application/component/sample/view/sample_layout.js
@@ -11,6 +11,10 @@ define('application/component/sample/view/sample_layout',
         var SampleView = MA_layout.extend({
             template: "subdir/tpl",
             initialize: function(){
+
+                this.model = new Backbone.Model;
+                this.model.set('message', 'this is a message from backbone model');
+
                 MA_layout.prototype.initialize.apply(this, arguments);
             },
             behaviors: {
