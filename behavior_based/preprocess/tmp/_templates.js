0d58ebcf95f3b699e083b8a113544714e30ba322
s.savajols@mazarinedigital.com <s.savajols@gmail.com>
2014-11-06 09:28:35 +0100

    clean

--- behavior_based/preprocess/tmp/_templates.js
+++
@@ -1,4 +0,0 @@
-window.twigTemplates = window.twigTemplates || {};
-window.twigTemplates["subdir/tpl"] = Twig.twig({ data: [{"type":"raw","value":"<span>ABC</span>\nhhh ho ho ho\n<hr/>\n<a href=\"#\" class=\"action\">"},{"type":"output","stack":[{"type":"Twig.expression.type.variable","value":"message","match":["message"]}]},{"type":"raw","value":"</a>"}] });
-window.twigTemplates["test-2"] = Twig.twig({ data: [{"type":"raw","value":"<div>blabla "},{"type":"output","stack":[{"type":"Twig.expression.type.variable","value":"message","match":["message"]}]},{"type":"raw","value":" test</div>\n<hr/>\n<br/>\nHAHAHA\n<br/>"}] });
-window.twigTemplates["test"] = Twig.twig({ data: [{"type":"raw","value":"<div></div>"}] });
--- extend_based/preprocess/tmp/_twig.js
+++
@@ -1,4 +0,0 @@
-window.twigTemplate = window.twigTemplate || {};
-window.twigTemplate["subdir/tpl"] = Twig.twig({ data: [{"type":"raw","value":"<span>ABC</span>\nhhh ho ho ho\n<hr/>\n<a href=\"#\" class=\"action\">"},{"type":"output","stack":[{"type":"Twig.expression.type.variable","value":"message","match":["message"]}]},{"type":"raw","value":"</a>"}] });
-window.twigTemplate["test-2"] = Twig.twig({ data: [{"type":"raw","value":"<div>blabla "},{"type":"output","stack":[{"type":"Twig.expression.type.variable","value":"message","match":["message"]}]},{"type":"raw","value":" test</div>\n<hr/>\n<br/>\nHAHAHA\n<br/>"}] });
-window.twigTemplate["test"] = Twig.twig({ data: [{"type":"raw","value":"<div></div>"}] });
